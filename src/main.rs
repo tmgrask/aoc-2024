a14ab21bc8008d5183d7c61f7359130fbed727e0
tmgrask <t.mackersy@psiphon.ca>
2024-12-03 09:04:14 -0500

    day three

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 mod one;
 mod two;
+mod three;
 
 fn main() {
-    println!("day 1, part 1: {}", one::part_one());
-    println!("day 1, part 2: {}", one::part_two());
+    println!("\nDay 1"); // List sorting
+    println!("\tpart 1: {}", one::part_one());
+    println!("\tpart 2: {}", one::part_two());
 
-    println!("day 2, part 1: {}", two::part_one());
-    println!("day 2, part 1: {}", two::part_two());
+    println!("\nDay 2"); // Order parsing with dropout
+    println!("\tpart 1: {}", two::part_one());
+    println!("\tpart 2: {}", two::part_two());
+
+    println!("\nDay 3"); // Regexing multiple captures
+    println!("\tpart 1: {}", three::part_one());
+    println!("\tpart 2: {}", three::part_two());
 }
---
+++ src/three/mod.rs
@@ -0,0 +1,76 @@
+use regex::{Regex, Captures};
+use std::fs;
+
+pub fn part_one() -> i32 {
+    let content = fs::read_to_string("src/three/input.txt").expect("Should read");
+
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let mut total = 0;
+
+    for capture in re.captures_iter(&content) {
+        let a = &capture[1].parse::<i32>().unwrap();
+        let b = &capture[2].parse::<i32>().unwrap();
+        total += a * b;
+    }
+
+    return total;
+}
+
+#[derive(Debug)]
+enum Match {
+    Mul { a: i32, b: i32 },
+    Do {},
+    Dont {},
+}
+
+impl From<Captures<'_>> for Match {
+    fn from(capture: Captures<'_>) -> Self {
+        if let Some(_) = capture.name("mul") {
+            Match::Mul {
+                a: capture[2].parse::<i32>().unwrap(),
+                b: capture[3].parse::<i32>().unwrap(),
+            }
+        } else if let Some(_) = capture.name("do") {
+            Match::Do {}
+        } else if let Some(_) = capture.name("dont") {
+            Match::Dont {}
+        } else {
+            panic!("unknown match captured!")
+        }
+    }
+}
+
+pub fn part_two() -> i32 {
+    let content = fs::read_to_string("src/three/input.txt").expect("Should read");
+
+    let re = Regex::new(r"(?P<mul>mul\((\d+),(\d+)\))|(?P<do>do\(\))|(?P<dont>don't\(\))").unwrap();
+
+    let mut total = 0;
+    let mut enabled = true;
+
+    for capture in re.captures_iter(&content) {
+        let m: Match = capture.into();
+        
+        match m {
+            Match::Mul { a, b } => {
+                if enabled {
+                    total += a * b;
+                }
+            }
+            Match::Do {} => {
+                if !enabled {
+                    enabled = true;
+                }
+            }
+            Match::Dont {} => {
+                if enabled {
+                    enabled = false;
+                }
+            }
+
+        }
+
+    }
+    return total;
+}
